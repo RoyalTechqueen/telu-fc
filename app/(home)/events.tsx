cb90b4f15fd848e3f63fbc067ec682c7e22944e9
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2024-09-12 21:24:48 +0100

    change

--- app/(home)/events.tsx
+++ app/(home)/events.tsx
@@ -13,10 +13,30 @@ const AnniversaryEvent: React.FC = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -27,7 +47,10 @@ const AnniversaryEvent: React.FC = () => {
       <div className="container mx-auto px-4 mb-8">
         <Slider {...sliderSettings}>
           {images.map((src, index) => (
-            <div key={index} className="relative w-full h-[700px]">
+            <div
+              key={index}
+              className="relative w-full h-48 md:h-[400px] xl:h-[700px]"
+            >
               <Image
                 src={src}
                 alt={`Anniversary Image ${index + 1}`}
@@ -41,7 +64,7 @@ const AnniversaryEvent: React.FC = () => {
       </div>
       <div className="container mx-auto px-4 py-8">
         <div className="shadow-xl rounded-lg p-8 border border-gray-200">
-          <h2 className="text-5xl font-extrabold mb-6 text-gray-800 text-center">
+          <h2 className="text-2xl lg:text-5xl font-extrabold mb-6 text-gray-800 text-center">
             Join Us for Telu FC&apos;s Spectacular 5th Anniversary Celebration!
           </h2>
           <h3 className="text-3xl mb-6 text-gray-800">Event Details</h3>
